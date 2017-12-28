9e38eef46a6e07e2141d710e615731a0e8f58215
Dean Moses <moses@tacocat.com>
2017-12-28 12:01:23 -0800

    Adding vscode editor configuration

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { App } from "./components/App";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+//import { Provider } from 'react-redux';
+import { App } from './components/App';
 
 ReactDOM.render(
     <App />,
-    document.getElementById("app")
+    document.getElementById('app')
 );
