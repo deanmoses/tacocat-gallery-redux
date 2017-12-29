d074108c05a06b2578a854c0932329312061370b
Dean Moses <moses@tacocat.com>
2017-12-28 23:29:18 -0800

    Added first draft of Site shell components

---
+++ src/components/Site.tsx
@@ -0,0 +1,69 @@
+/**
+ * React.js components that render the site's shell
+ */
+
+import * as React from 'react';
+import Config from '@src/config';
+
+/**
+ * React.js component that renders the site shell
+ */
+interface PageProps {
+	classes?: string;
+	showFooter?: boolean;
+}
+export const Page: React.StatelessComponent<PageProps> = ({
+	children,
+	classes = '',
+	showFooter = true
+}) => (
+	<div>
+		<div className={classes}>{children}</div>
+		{showFooter && (
+			<div className="footer hidden-xs hidden-sm">
+				<img src="images/tacocat-logo.png" width="102px" height="19px" />
+			</div>
+		)}
+	</div>
+);
+
+/**
+ * React.js component that renders the site's header bar with title
+ */
+export interface HeaderTitleProps {
+	title?: string;
+	href?: string;
+	showTitle?: boolean;
+}
+export const HeaderTitle: React.StatelessComponent<HeaderTitleProps> = ({
+	title = '',
+	href = '',
+	showTitle = true
+}) => {
+	var derivedTitle = (
+		<a className="navbar-brand" href={href}>
+			{title}
+		</a>
+	);
+
+	return (
+		<div>
+			<nav className="header navbar" role="navigation">
+				<div className="navbar-header">{derivedTitle}</div>
+				<div className="header-controls hidden-xxs">
+					{showTitle && (
+						<span className="hidden-xs site-title">{Config.siteTitle()}</span>
+					)}
+					{/*props.showSearch && (
+						<span className="search-button">
+							<Site.SearchButton returnPath={props.path} />
+						</span>
+					)*/}
+				</div>
+			</nav>
+			{
+				//<Site.HeaderButtons>{props.children}</Site.HeaderButtons>
+			}
+		</div>
+	);
+};
