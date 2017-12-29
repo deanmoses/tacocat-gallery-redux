34be2a36fd2520c565bcf6a22ca3a2a140d37814
Dean Moses <moses@tacocat.com>
2017-12-28 23:29:58 -0800

    App uses new Album and Not Found components

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,13 +3,9 @@
  */
 
 import * as React from 'react';
-import {
-	HashRouter as Router,
-	Route,
-	RouteComponentProps,
-	Switch,
-	Link
-} from 'react-router-dom';
+import { HashRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import * as Album from '@src/components/Album';
+import { NotFoundPage } from '@src/components/NotFound';
 
 /**
  * Root React.js component of the application
@@ -47,43 +43,15 @@ export const App: React.StatelessComponent = () => (
                 A <Route> with no path always matches.
             */}
 			<Switch>
-				<Route exact path="/" component={HomePage} />
-				<Route exact path="/one" component={One} />
-				<Route exact path="/two" component={Two} />
-				<Route exact path="/(\\d\\d\\d\\d)" component={YearAlbumPage} />
+				<Route exact path="/" component={Album.RootAlbumPage} />
+				<Route exact path="/(\\d\\d\\d\\d)" component={Album.YearAlbumPage} />
 				<Route
 					exact
 					path="/(\\d\\d\\d\\d/\\d\\d\-\\d\\d)"
-					component={WeekAlbumPage}
+					component={Album.WeekAlbumPage}
 				/>
 				<Route component={NotFoundPage} />
 			</Switch>
 		</div>
 	</Router>
-);
-
-const HomePage: React.StatelessComponent<RouteComponentProps<any>> = () => (
-	<h1>Home</h1>
-);
-
-const One: React.StatelessComponent<RouteComponentProps<any>> = () => (
-	<h1>One</h1>
-);
-
-const Two: React.StatelessComponent<RouteComponentProps<any>> = () => (
-	<h1>Two</h1>
-);
-
-const YearAlbumPage: React.StatelessComponent<
-	RouteComponentProps<any>
-> = () => <h1>Year</h1>;
-
-const WeekAlbumPage: React.StatelessComponent<
-	RouteComponentProps<any>
-> = () => <h1>Week</h1>;
-
-const NotFoundPage: React.StatelessComponent<RouteComponentProps<any>> = () => (
-	<h1>
-		Not found: <code>{location.pathname + location.hash}</code>
-	</h1>
 );
