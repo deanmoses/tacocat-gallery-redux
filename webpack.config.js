f2c5e06a65a94728fc71d1bf7ae99c85f8ee5bae
Dean Moses <moses@tacocat.com>
2017-12-28 17:17:43 -0800

    import via @src
    Settled on an import syntax:  import xxx from '@src/someSrcFile’;

--- webpack.config.js
+++ webpack.config.js
@@ -1,65 +1,68 @@
 const webpack = require('webpack');
 const path = require('path');
 
-// This helper function is not strictly necessary.
-// I just don't like repeating the path.join a dozen times.
+const PATH_TO_SRC = path.join(__dirname, 'src');
+
+/**
+ * Helper function to avoid repeating path.join a dozen times
+ */
 function srcPath(subdir) {
-    return path.join(__dirname, "src", subdir);
+	return path.join(PATH_TO_SRC, subdir);
 }
 
 var config = {
-    // context: the base directory, an absolute path, for resolving entry points and loaders from configuration
-    context: __dirname + '/src',
+	// The base directory, an absolute path, for resolving entry points and loaders from configuration
+	context: PATH_TO_SRC,
 
-    // entry: the point or points to enter the application. At this point the application starts executing. 
-    entry: "./index.tsx",
+	// The entry point into the application. At this point the application starts executing
+	entry: './index.tsx',
 
-    output: {
-        // path: the output directory as an absolute path
-        path: __dirname + "/app",
+	output: {
+		// The output directory as an absolute path
+		path: __dirname + '/app',
 
-        // filename: the name of the output bundle
-        filename: "bundle.js"
-    },
+		// The name of the output bundle
+		filename: 'bundle.js'
+	},
 
-    plugins: [
-        new webpack.DefinePlugin({
-            ON_TEST: process.env.NODE_ENV === 'test'
-        })
-    ],
+	plugins: [
+		new webpack.DefinePlugin({
+			ON_TEST: process.env.NODE_ENV === 'test'
+		})
+	],
 
-    resolve: {
-        // Needed so files can import from 'components/App' instead of './components/App'
-        alias: {
-            actions: srcPath('actions'),
-            components: srcPath('components')
-        },
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+	resolve: {
+		// Enable files to import from '@src/components/App' instead of './components/App'
+		// These work with the 'path' setting in tsconfig.json
+		alias: {
+			'@src': PATH_TO_SRC
+		},
+		// Add '.ts' and '.tsx' as resolvable extensions
+		extensions: ['.ts', '.tsx', '.js', '.json']
+	},
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+	module: {
+		rules: [
+			// All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'
+			{ test: /\.tsx?$/, loader: 'awesome-typescript-loader' },
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
-            { test: /\.html$/, loader: 'raw', exclude: /node_modules/ },
-            { test: /\.css$/, loader: 'style!css', exclude: /node_modules/ },
-            { test: /\.styl$/, loader: 'style!css!stylus', exclude: /node_modules/ }
-        ]
-    },
+			// All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'
+			{ enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+			{ test: /\.html$/, loader: 'raw', exclude: /node_modules/ },
+			{ test: /\.css$/, loader: 'style!css', exclude: /node_modules/ },
+			{ test: /\.styl$/, loader: 'style!css!stylus', exclude: /node_modules/ }
+		]
+	},
 
-    devServer: {
-        contentBase: path.join(__dirname, "src")
-    }
+	devServer: {
+		contentBase: PATH_TO_SRC
+	}
 };
 
 if (process.env.NODE_ENV === 'production') {
-    config.output.path = __dirname + '/dist';
-    config.plugins.push(new webpack.optimize.UglifyJsPlugin());
-    config.devtool = 'source-map';
+	config.output.path = __dirname + '/dist';
+	config.plugins.push(new webpack.optimize.UglifyJsPlugin());
+	config.devtool = 'source-map';
 }
 
 module.exports = config;
