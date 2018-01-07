edf889425d14a02414226ff37cc050f33f5b030c
Dean Moses <moses@tacocat.com>
2018-01-07 12:25:16 -0800

    Removed inaccurate comment

--- src/models/album-root.ts
+++ src/models/album-root.ts
@@ -4,10 +4,6 @@ import DateBasedAlbum from '@src/models/album-datebased';
  * Overrides the default album class with  behavior specific to year albums.
  */
 export default class RootAlbum extends DateBasedAlbum {
-	//
-	// TODO: it doesn't seem pageTitle() is used, can I delete this class?
-	//
-
 	/**
 	 * Friendly title of page
 	 */
